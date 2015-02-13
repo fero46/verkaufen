ec0915cb74a079624b5de67e8d6306c2eb391ece
Prelang Builder <builder@prelang.com>
2015-02-13 13:50:25 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,5 +1,5 @@
 class AdsController < ApplicationController
-  before_action :set_ad, only: [:show, :edit, :update, :destroy]
+  before_action :set_ad, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /ads
   # GET /ads.json
